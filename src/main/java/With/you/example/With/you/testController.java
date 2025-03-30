da7b32ca3a78efd9569976346da1ec460750f297
김진태 <aasdxcz123@gmail.com>
2025-03-31 02:40:24 +0900

    init: jsp 연동

--- src/main/java/With/you/example/With/you/testController.java
+++ src/main/java/With/you/example/With/you/testController.java
@@ -1,13 +1,14 @@
 package With.you.example.With.you;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class testController {
 
     @GetMapping("/test")
     public String test() {
+        System.out.println("test");
         return "test";
     }
 }
